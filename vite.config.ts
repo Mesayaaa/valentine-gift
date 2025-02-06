76423c340d753dddafc32feb821ae893040ee9a9
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2025-02-06 16:01:55 +0700

    Fix base URL and build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: "/will-you-be-my-valentine/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
 });
