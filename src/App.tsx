6f01cd09dc5f020cb7d9e1c69bd87c82605ec620
Mesayaaa <mesayachristopher2112@gmail.com>
2025-02-11 01:55:33 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ReactConfetti from "react-confetti";
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
-  const [open, setOpen] = useState(false);
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
   const yesButtonSize = noCount * 20 + 16;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleNoClick = () => {
     setNoCount(noCount + 1);
@@ -41,95 +56,61 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-0 bg-white">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-white overflow-hidden">
+      {yesPressed && (
+        <ReactConfetti width={windowSize.width} height={windowSize.height} />
+      )}
+
       {yesPressed ? (
         <>
-          <div>
-            <span className="text-sm animate-bounce text-black">
+          <div className="animate-bounce">
+            <span className="text-sm text-black">
               Susahnya dapet stiker ini sayang alamakkk
             </span>
           </div>
           <img
-            className="max-w-full h-auto transition-transform transform hover:scale-110"
+            className="max-w-full h-auto"
             src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
             alt="Bear Kiss"
           />
-          <div className="my-4 text-2xl md:text-4xl font-bold text-center text-black">
+          <div className="my-4 text-2xl md:text-4xl font-bold text-center text-black animate-pulse">
             <span>WOKEEEH LOVE U SAYANG, OIYA WAIT MAS PUNYA INI!!</span>
           </div>
-          <div>
-            <div className="relative inline-block text-left">
-              <div>
-                <button
-                  type="button"
-                  className="mt-4 rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-800 transition duration-300 transform hover:scale-105"
-                  onClick={() =>
-                    (window.location.href =
-                      "https://mesayaaa.github.io/image-slider/")
-                  }
-                >
-                  pencet deh sayang
-                </button>
-              </div>
-
-              {open && (
-                <div className="absolute left-1/2 transform -translate-x-1/2 z-10 mt-2 w-auto max-w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 px-4 max-h-screen overflow-y-auto">
-                  <div className="py-1 text-center">
-                    <span className="block text-lg font-bold mb-2 whitespace-nowrap overflow-hidden">
-                      kita mau pergi keee
-                    </span>
-                    <div className="flex items-center justify-center gap-2">
-                      <div className="flex flex-col space-y-4">
-                        <button
-                          className="px-6 py-3 text-lg text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition duration-200"
-                          onClick={() => alert("makasii sayang")}
-                        >
-                          karaokee
-                        </button>
-                        <button
-                          className="px-6 py-3 text-lg text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition duration-200"
-                          onClick={() => alert("Mmakasii sayang")}
-                        >
-                          kidzaniaaa
-                        </button>
-                        <button
-                          className="px-6 py-3 text-lg text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition duration-200"
-                          onClick={() => alert("Mmakasii sayang")}
-                        >
-                          pantaiiii
-                        </button>
-                      </div>
-                    </div>
-                    <span className="block text-lg px-4 py-2 text-sm text-black">
-                      see u sayang!!! &lt;3
-                    </span>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
+          <button
+            type="button"
+            className="mt-4 rounded-full bg-blue-600 px-6 py-3 font-bold text-white hover:bg-blue-800 transition-all duration-300 transform hover:scale-105 hover:shadow-xl"
+            onClick={() =>
+              (window.location.href =
+                "https://mesayaaa.github.io/travel-places/")
+            }
+          >
+            pencet deh sayang ❤️
+          </button>
         </>
       ) : (
         <div className="flex flex-col items-center justify-center">
           <img
-            className="h-[200px] max-w-full h-auto transition-transform transform hover:scale-80"
+            className="h-[200px] bg-white"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
             alt="Cute Love Bear"
           />
-          <h1 className="my-4 text-2xl md:text-4xl text-center text-black">
-            Km mau pergi sama mas nda?
+          <h1 className="my-4 text-2xl md:text-4xl text-center text-black font-bold">
+            Km mau pergi sama mas nda? 🥺
           </h1>
-          <div className="flex flex-col md:flex-row items-center justify-center">
+          <div className="flex flex-col md:flex-row items-center justify-center gap-4">
             <button
-              className={`mr-0 md:mr-4 rounded bg-green-600 px-4 py-2 font-bold text-white hover:bg-green-800 transition duration-300 transform hover:scale-105`}
-              style={{ fontSize: yesButtonSize }}
+              className="transform transition-all duration-300 hover:scale-110 rounded-full bg-green-600 px-8 py-3 font-bold text-white hover:bg-green-800 hover:shadow-xl"
+              style={{
+                fontSize: yesButtonSize,
+                transition: "all 0.3s ease",
+              }}
               onClick={handleYesClick}
             >
               MAUUU
             </button>
             <button
               onClick={handleNoClick}
-              className="mt-4 md:mt-0 rounded bg-red-600 px-4 py-2 font-bold text-white hover:bg-red-800 transition duration-300 transform hover:scale-105"
+              className="transform transition-all duration-300 hover:scale-105 rounded-full bg-red-600 px-8 py-3 font-bold text-white hover:bg-red-800 hover:shadow-xl"
             >
               {noCount === 0 ? "ga" : getNoButtonText()}
             </button>
