2d2f140f896ebc59c75f1bbd4c9624af98f3da9e
Mesayaaa <mesayachristopher2112@gmail.com>
2025-02-12 21:41:29 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine/",
+  base: "/buat-km/",
   plugins: [react()],
   build: {
     outDir: "dist",
