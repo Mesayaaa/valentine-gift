f0d7b2433e71a33478761b97004c09978c615f48
Mesayaaa <mesayachristopher2112@gmail.com>
2025-07-30 09:51:00 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/valentine-gift/",
+  base: "/",
   plugins: [react()],
   build: {
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  preview: {
+    port: 3001,
   },
 });
